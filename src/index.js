f75127dd619ee2137fc02d7b4b0ca79d878f495c
Sheshasena Reddy <53706557+sheshu22@users.noreply.github.com>
2020-12-22 17:52:58 +0530

    initial chanages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+//import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
+
+ReactDOM.render(<h1>hello world!</h1>, document.getElementById("root"));
